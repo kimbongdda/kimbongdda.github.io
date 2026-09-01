65486f9cf411e392b118dfa8ff0a703078c32dd8
JAEYONG KIM <widyddl11123@gmail.com>
2026-09-01 17:01:18 +0900

    Add files via upload

---
+++ dl-config.js
@@ -0,0 +1,8 @@
+window.DL_CONFIG = {
+  "v": 1,
+  "iter": 250000,
+  "salt": "XEFxvHJOUV/wX9YOskcryg==",
+  "iv": "34U/iazE6Ysh22xk",
+  "data": "8vrfP8kkGDhDeReGJr7dOVvXF/LlQLlvpDkTNzy2aqReILFRQPSf8U0BABdvrjXbxeamlliNJOw0bK88tJ7s6bTQU6hhcU82wWxOQmKYnyiHArHNFLNajmZrw9z6JfxfaGOFO76pt//KxLdIUtjf3qg6URFc9WVWH9UK9uy1ehVJ8Ago+kE7/es=",
+  "updated": "2026-09-01"
+};
