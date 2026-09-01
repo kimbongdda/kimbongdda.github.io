2ceff150f8c3bb1b54b90f3f47f292778ef2d7ed
JAEYONG KIM <widyddl11123@gmail.com>
2026-09-01 17:56:12 +0900

    Add files via upload

--- dl-config.js
+++ dl-config.js
@@ -1,8 +1,8 @@
 window.DL_CONFIG = {
   "v": 1,
   "iter": 250000,
-  "salt": "ytdqmJLeQHnPzQXN06RvLQ==",
-  "iv": "1JPvkfkeyJCbRXe3",
-  "data": "+gUYavtygZbz6fLoJHUWXiCidnSQQl/smR9kyaSTejvgfR0ecND7aRrlLh01fU3m4KIqR7L8fun6potQvUpW7fdAkeO0Jg8XgUwB6a7aeBOLtvnLC2ld1RypqLOYd0DYHPKyLknHo0aeSSPgW7U/TXgbiW+s6B1tw2ik9d/FJzlJMcRmUhrvzV2mMpDVMlqKtUZxULbflw71goO/v0OhXzgD65dCMNDTdYP6qFKOZOWAajUtnldDmpx7WvbpIlX7ydLehZsYI6xMPZKZ3VgMsuU6TftYRyxHp734Na5W4vSRMOrPZjApri38MHfYyKrN5qgTeyMZb0RYo1ZPKDXH73yJEk565jVpfFjGMErXjKnq8yRRn3be",
+  "salt": "RVdN45Key9kvBWWLle8aMg==",
+  "iv": "LEVlXMpFYHYGPGKl",
+  "data": "7xDIeKYZKVPP5kwbWVE13JxHxVhebFmlU+2g5gbvnohvW57c0bf05LTVAy1X/iEM7VkN4ikCix1Vrcoz1Sj7v9jfKgkxwnMSCKI8GkkXeLct6Cxcw6zdJYFSrBb+WcbemEwkNwelIYYFA2GI26MR8+X366iQQEVYTY5zocZKUFf3nvrHZivIB31sqHMnjdPWyxxtQWZ8G+ELTGsQOKnX5Y4oGbRFBO074oJKciz3vaLtgjgvMM0V8MY9FYa5BUNws/SrGrero6gGMXb5Hv0ewK/vy3gd2shlWEr6IKOoQFpnBFvZYXUtnjAA7ZofMPi0masTXa/YuCwzaL9/v/mGYPLyyD/7yZt77pZp2oh/vhWx4o4xI7Wz",
   "updated": "2026-09-01"
 };
