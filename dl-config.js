af8d3e153387ceee09fe9038996aafc5105345ff
JAEYONG KIM <widyddl11123@gmail.com>
2026-09-02 14:19:50 +0900

    오늘보드 1.1.0 배포

--- dl-config.js
+++ dl-config.js
@@ -1,8 +1,8 @@
 window.DL_CONFIG = {
   "v": 1,
   "iter": 250000,
-  "salt": "RVdN45Key9kvBWWLle8aMg==",
-  "iv": "LEVlXMpFYHYGPGKl",
-  "data": "7xDIeKYZKVPP5kwbWVE13JxHxVhebFmlU+2g5gbvnohvW57c0bf05LTVAy1X/iEM7VkN4ikCix1Vrcoz1Sj7v9jfKgkxwnMSCKI8GkkXeLct6Cxcw6zdJYFSrBb+WcbemEwkNwelIYYFA2GI26MR8+X366iQQEVYTY5zocZKUFf3nvrHZivIB31sqHMnjdPWyxxtQWZ8G+ELTGsQOKnX5Y4oGbRFBO074oJKciz3vaLtgjgvMM0V8MY9FYa5BUNws/SrGrero6gGMXb5Hv0ewK/vy3gd2shlWEr6IKOoQFpnBFvZYXUtnjAA7ZofMPi0masTXa/YuCwzaL9/v/mGYPLyyD/7yZt77pZp2oh/vhWx4o4xI7Wz",
-  "updated": "2026-09-01"
+  "salt": "6BqEZOBMeGuvoBdS4N1dVg==",
+  "iv": "Z+pQ6h/HwgmDCnsS",
+  "data": "TNtZTSrXVV06NxrWK6Op6qNLOuo18gjxfGd7nTnjCDPwFfnf4RJ9nVoDMMGeT0LdSQBD3qjDETQWkQnIpFrkbVIFcV3xccSawc6GNPdlAdRNNW8JMzUa1KQq/3kfkihQCBeIwkym4Da8Q0bXbWijEvKIvRKLiLp1Zft7aOWLfTqXRiYdODym/vTAg3FlYTKo3l+VAuu1UOc7Z+9Q+Ld2OQseVZJg6/SrGyRCHeTeD3+bsdkxXpDklU1VcG0gVKcpdRq6iWcYH6NEZc7B7riXr6gEolyXvqAKzHu1z4YhwbGnzYhlroizLyOT+dFWtffn9l31HJQYpU7jEEzQljnchmBdt6i7PpExVmYKjUFeSnD5+kXEkrS6",
+  "updated": "2026-09-02"
 };
